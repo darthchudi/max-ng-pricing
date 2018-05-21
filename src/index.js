64802ac23ccee9159bb4df13dac80d3db713b971
darthchudi <chukwudioranu@ymail.com>
2018-05-21 18:47:41 +0100

    fix: fix issue with google maps not loading

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
 
 import './includes/bootstrap';
 import './styles/pricing.css';
 
 ReactDOM.render((
-	<HashRouter>
+	<BrowserRouter>
 		<div>
 			<Switch>
 				<Route exact path="/" component={App}/>
 			</Switch>
 		</div>
-	</HashRouter>
+	</BrowserRouter>
 	), document.getElementById('root'));
 registerServiceWorker();
